5ce49bb8aa78a76fbba9a3b719080f508b2048d6
Gixx <navig80@gmail.com>
2019-10-02 14:14:37 +0200

    Add proper privacy policy page with feature toggle.

---
+++ docs/assets/js/site.js
@@ -0,0 +1,31 @@
+const PRIVACY_DISQUS_COOKIE_NAME = 'privacy_DisqusEnabled';
+
+function isDisqusEnabled()
+{
+    return getCookie(PRIVACY_DISQUS_COOKIE_NAME) === 'Yes';
+}
+
+function setCookie(cname, cvalue, exdays)
+{
+    let date = new Date();
+    date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+ date.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname)
+{
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieArray = decodedCookie.split(';');
+    for(let i = 0, num = cookieArray.length; i < num; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return '';
+}
